96596cecd34b3dadd055724a355b1ad73c67f99c
laboratorioesan <laboratorioesan@esan.edu.pe>
2025-11-21 19:17:20 -0500

    Primer commit

---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'nuevoregistro', component: () => import('src/components/auth/RegisterForm.vue') },
+      { path: 'products', component: () => import('pages/ProductPage.vue') },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('src/components/auth/LoginForm.vue'),
+  },
+  {
+    path: '/register',
+    component: () => import('src/components/auth/RegisterForm.vue'),
+  },
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+]
+
+export default routes
